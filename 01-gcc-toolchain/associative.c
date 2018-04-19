70af1bf7503808e3b3d2e70eba26c338980baa6b
Norbert Pozar <bertapozar@gmail.com>
2018-04-19 16:52:46 +0900

    update

--- 01-gcc-toolchain/associative.c
+++ 01-gcc-toolchain/associative.c
@@ -1,3 +1,7 @@
+/**
+ * Illustration of rounding errors and dependence of a result of finite
+ * precision arithmetics on the order of operations.
+ */
 #include <stdio.h>
 
 int main()
@@ -5,10 +9,8 @@ int main()
     double a = 1.0e16;
     double b = -1.0e16;
     double c = 1.0;
-    double d = 0.1;
 
     printf("(a + b) + c = %f\n", (a + b) + c);
     printf("a + (b + c) = %f\n", a + (b + c));
-    printf("%f\n", (d * 10 - 1.)*1e15);
     return 0;
 }
--- 01-gcc-toolchain/warns.c
+++ 01-gcc-toolchain/warns.c
@@ -1,7 +1,12 @@
+/**
+ * gcc by default does not warn even when some kinds of serious bugs are present.
+ *
+ * Use -Wall -Wextra command line flags when compiling!
+ */
 #include <stdio.h>
 int main() {
     double x;
-    printf("%f\n", x);  // unitialized variable
+    printf("%f\n", x);  // unitialized variable (Undefined Behavior!)
 }
 
 int n(int i) {
