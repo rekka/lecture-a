c460937bb249b78ec3c16b82265c094623f2f007
Norbert Pozar <bertapozar@gmail.com>
2018-04-26 15:31:02 +0900

    Lecture 2

---
+++ 02-shell/hello.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# print something
+echo "Hello, $1!"
---
+++ 02-shell/read_stdin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i;
+    double x;
+    cout << "Enter two numbers" << endl;
+    cin >> i >> x;
+    cout << "Sum = " << i + x << endl;
+    return 0;
+}
